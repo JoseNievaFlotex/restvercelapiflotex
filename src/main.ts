6a795c3aeff5a9e61feb0655febecd2912d29eac
joseNievaFlotex <jose.nieva@flotex.pe>
2022-11-04 09:44:59 -0500

    cortafuegos

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('flotex-api', app, document);
 
-  await app.listen(process.env.PORT ||  3000);
+  await app.listen(process.env.PORT ||  8080);
 }
 bootstrap();
